583ef1cf623c902984e1c79634e3f26eed104aa8
Chet Mancini <chetmancini@gmail.com>
2012-04-22 17:07:13 -0400

    replace

--- code/MRProj.java
+++ code/MRProj.java
@@ -1,10 +1,21 @@
+import java.io.*;
+
 public class MRProj{
+
+	String productionFile = "../data/production.txt";
+	FileInputStream fs = new FileInputStream(productionFile);
+	DataInputStream in = new DataInputStream(fs);
+	BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
 	// compute filter parameters for netid ajd28
 	float fromNetID = 0.974;
 	float desiredDensity = 0.59;
 	float wMin = 0.4 * fromNetID;
-	float wLimit = wMin + desiredDensity; 
+	float wLimit = wMin + desiredDensity;
+
+	public float parseLine(String line){
+		return Float.parseFloat(line.replace("\n", ""))
+	}
 
 	// assume 0.0 <= wMin <= wLimit <= 1.0
 	public boolean getNextFilteredInput() {
@@ -13,7 +24,7 @@ public class MRProj{
 	}
 
 
-	public void run(){
+	public void load(int N){
 		for( int j = 0; j < N; j++ ) {
 		    for( int i = 0; i < j; i++ ) {
 		        a[i, j] = getNextFilteredInput();
