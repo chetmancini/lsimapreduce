798b2bfa6b590b4617ceba707c5c0c5960ee2f71
sean <positivelyskewed.sf@gmail.com>
2012-04-27 15:24:56 -0400

    fixed bugs.  added snake.

--- code/lsimr/src/main/java/org/lsi/unionfind/UnionFind.java
+++ code/lsimr/src/main/java/org/lsi/unionfind/UnionFind.java
@@ -43,19 +43,36 @@ public class UnionFind {
         //This looks redundant, but we look both ways even
         //if left is clearly lower, just so that we can also
         //count the vertices as we go.
-        for(int j=0; j<2; ++j)
+        //
+        //Also, we make two passes.  First pass, we initialize
+        //the parents of every vertex by inspecting the left and
+        //down vertices from them and seeing if either is a lower
+        //index.  If they are different, unite them and choose
+        //the lower root.
+        //
+        //While uniting, we run up the tree and replace the roots
+        //all the way up.  Note that we only replace the roots, not
+        //the vertices that might also be children of that root we
+        //replaced.
+        //
+        //The second pass, it does exactly the same, but now the
+        //the roots (which are scanned first since we go from smallest
+        //to highest), will all be of the minimum index, so we use the
+        //second pass to make sure all children are filled in consistently.
+        for(int j=0; j<2; ++j){
             for(int i=0; i<m_id.length; ++i)
             {
                 if(m_id[i] > 0)
                 {
-                    lookUp(i);
-                    lookRight(i);
+                    lookLeft(i);
+                    lookDown(i);
                 }
-            };
+                
+            }
+        }
 
-        //TODO: This is correct but sloppy.  We double count
-        //every vertex, so have to devide by two here.
-
+        //We double count every vertex when we make two
+        //passes, so have to devide by two here.
         m_edges = m_edges/2;
     }
 
@@ -68,23 +85,87 @@ public class UnionFind {
     }
 
     /**
-     * Return the id of the element to the left, if there
+     * Return the id of the element below, if there
      * is a tree there, otherwise return own id.
-     * @param i The index of the point of interest.
+     * @param i The zero based index of the point of interest.
      */
-    private void lookRight(int i)
+    private void lookLeft(int i)
     {
-        //if i'm not the right column, and the spot right of me
-        //has a tree.
-        if(i+m < m_id.length && m_id[i+m] > 0){
-            ++m_edges;  //there is an edge.
-            if(m_id[i+m] > m_id[i])
-                m_id[i+m]=m_id[i];
-            else{
-                m_id[m_id[i] - 1]=m_id[i+m];
-                m_id[i]=m_id[i+m];
-            }
+        //if spot above me is a tree, and i'm not in the
+        //top row.
+        if(isTree(i-m)){
+            ++m_edges; //there is an edge.
+            unite(i+1, i+1-m);  //converted to one based.
         }
+    }
+
+    
+    /**
+     * Return the id of the element below, if there
+     * is a tree there, otherwise return own id.
+     * @param i The zero based index of the point of interest.
+     */
+    private void lookDown(int i)
+    {
+        //if spot above me is a tree, and i'm not in the
+        //top row.
+        if(isTree(i-1) && row(i) != 0){
+            ++m_edges; //there is an edge.
+            unite(i+1, i);  //converted to one based.
+        }
+    }
+
+
+    /**
+     * Unite p and q
+     * @param p the one based index of a point
+     * @param q the one based index of a point
+     */
+    private void unite(int p, int q)
+    {
+        System.out.println("Uniting " + p + ", " + q);
+        int i = root(p);
+        int j = root(q);
+        System.out.println("With roots " + i + ", " + j);
+        if(i < j)
+            m_id[j-1] = i;
+        else
+            m_id[i-1] = j;
+    }
+
+    /**
+     * Return the root of i.
+     * @param i the one based index of the point.
+     * @return the one based id of of the root of the tree
+     *         containing point i.
+     */ 
+    private int root(int i)
+    {
+        while(i != m_id[i-1]){
+            while(m_id[i-1] != m_id[m_id[i-1]-1])
+                m_id[i-1] = m_id[m_id[i-1]-1];
+            i = m_id[i-1];
+        }
+        return i;   
+    }
+
+    /**
+     * Conveniently detect if there is a vertex at i, and if so
+     * return true.  If i is out of bounds, or there is no vertex,
+     * return false.
+     * @param i The index of the point in question.
+     * @return true if there is a vertex, false otherwise.
+     */
+    private boolean isTree(int i)
+    {
+        if(i >= 0 && i < m_id.length && m_id[i] > 0)
+            return true;
+        return false;
+    }
+    
+    private int row(int i)
+    {
+        return i % m;
     }
 
     /**
--- code/lsimr/src/test/java/org/lsi/unionfind/UnionFindTest.java
+++ code/lsimr/src/test/java/org/lsi/unionfind/UnionFindTest.java
@@ -123,6 +123,61 @@ public class UnionFindTest
 
         assertArrayEquals(expected,uf.getRoots());
     }
+    
+    private void deleteFromToBy(int from, int to, int by, int[] vec){
+        //adjust for zero based index.
+        from=from-1;
+        to=to;
+        for(int i=from; i<to; i+=by){
+            vec[i]=0;
+        }
+    }
+    
+    public void testUnionFindSnake()
+    {
+        
+        int[] vec = new int[100];
+        //using 1 based index so that 0 means no trees.
+        for(int i = 0; i < 100; ++i)
+            vec[i]=i+1;
+        
+        //clear a path to disconnect a space.
+        deleteFromToBy(12,92,10, vec);
+        deleteFromToBy(4,84,10,vec);
+        vec[84]=0;
+        vec[65]=0;
+        vec[44]=0;
+        vec[25]=0;
+        vec[4]=0;
+        deleteFromToBy(17,97,10,vec);
+        deleteFromToBy(9,89,10,vec);
+        
+        int[] expected = new int[100];
+        for(int i=0; i<100; ++i){
+            if(vec[i]==0)
+                expected[i]=0;
+            else
+                expected[i]=1;
+        }
+
+        UnionFind uf = new UnionFind(vec, 10, 10);
+
+        /**
+         * Matrix that looks like:
+         * 10 20 30 40 50 60 70 80 90 100
+         *                             99
+         * 8  18 28 38 48 58 68 78 88  98
+         * 7  
+         * 6  16    36 46 56    76 86  96 
+         *    15 25 35    55 65 75     95
+         *                             94
+         * 3  13 23 33 43 53 63 73 83  93 
+         * 2  
+         * 1  11 21 31 41 51 61 71 81  91 
+         *
+         */
+        assertArrayEquals(expected,uf.getRoots());
+    }
 
     public void testEdgesOne()
     {
